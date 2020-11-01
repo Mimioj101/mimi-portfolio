1ebbea7261690308a1769e09ea5473a31183cafb
Mimioj101 <mimioj@gmail.com>
2020-11-01 12:25:29 -0500

    email is linked

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -3,8 +3,21 @@ import React from "react"
 class ContactMe extends React.Component {
     render() {
         return(
-            <div>
-                <h1>She can code</h1>
+            <div className="window" id="contact-me-window">
+                <div className="title-bar">
+                    <div className="title-bar-text">Contact Mimi</div>
+                    <div className="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div className="window-body">
+                    <p>Email: <a href="mailto: mimioj@gmail.com">mimioj@gmail.com</a></p>
+                    <p>Phone: 609-457-7718</p>
+                    <p>LinkedIn: https://www.linkedin.com/in/mimio/</p>
+                    <p>Github: https://github.com/Mimioj101</p>
+                </div>
             </div>
         )
     }
