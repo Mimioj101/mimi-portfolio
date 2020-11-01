4010a6cb79f8bea66b9dcd01f83513ed1b2ca600
Mimioj101 <mimioj@gmail.com>
2020-11-01 12:10:55 -0500

    contact me div created and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ContactMe from './ContactMe.js'
 
 function App() {
   return (
     <div className="container">
 	    <h1 id="mimi-ojserkis" data-text="Mimi Ojserkis">Mimi Ojserkis</h1>
+      <ContactMe />
     </div>
   );
 }
---
+++ src/ContactMe.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class ContactMe extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>She can code</h1>
+            </div>
+        )
+    }
+}
+
+export default ContactMe;
