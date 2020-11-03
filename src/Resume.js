fe7c4db8536edf2957865854aa11f39c047fc68e
Mimioj101 <mimioj@gmail.com>
2020-11-03 15:30:15 -0500

    resume drag & drop

--- src/Resume.js
+++ src/Resume.js
@@ -1,11 +1,71 @@
 import React from "react"
 
-export default class Resume extends React.Component {
+class Resume extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            diffX: 0,
+            diffY: 0,
+            dragging: false,
+            styles: {}
+        }
+
+        this._dragStart = this._dragStart.bind(this);
+        this._dragging = this._dragging.bind(this);
+        this._dragEnd = this._dragEnd.bind(this);
+    }
+
+    _dragStart(e) {
+        this.setState({
+            diffX: e.screenX - e.currentTarget.getBoundingClientRect().left,
+            diffY: e.screenY - e.currentTarget.getBoundingClientRect().top,
+            dragging: true
+        });
+    }
+
+    _dragging(e) {
+
+        if(this.state.dragging) {
+            var left = e.screenX - this.state.diffX;
+            var top = e.screenY - this.state.diffY;
+    
+            this.setState({
+                styles: {
+                    left: left,
+                    top: top
+                }
+            });
+        }
+    }  
+
+    _dragEnd() {
+        this.setState({
+            dragging: false
+        });
+    }
+
+
     render () {
         return(
-            <div>
-                <iframe src="MRO Resume 11-02.pdf" width="500px" height="700px"/>
+            // <div className="window" id="resume-window" style={this.state.styles} onMouseDown={this._dragStart} onMouseMove={this._dragging} onMouseUp={this._dragEnd}>
+                    
+            <div className="window" id="resume-window" style={this.state.styles} onMouseDown={this._dragStart} onMouseMove={this._dragging} onMouseUp={this._dragEnd}>
+                <div className="title-bar">
+                    <div className="title-bar-text">Resume</div>
+                    <div className="title-bar-controls">
+                    <button aria-label="Minimize"></button>
+                    <button aria-label="Maximize"></button>
+                    <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div className="window-body">
+                    <img src="resume-img.png" width="500px" />
+                    {/* <iframe title="Resume" src="MRO Resume 11-02.pdf" width="500px" height="700px"/> */}
+                </div>
             </div>
         )
     }
 }
+
+export default Resume;
