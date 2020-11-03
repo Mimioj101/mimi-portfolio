6b3d7e84edde9261c4f334e4c45b2a5f67fcbf08
Mimioj101 <mimioj@gmail.com>
2020-11-03 14:54:13 -0500

    drag & drop works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from "react"
 import './App.css';
 import ContactMe from './ContactMe.js'
 
-function App() {
-  return (
-    <div className="container">
-	    <h1 id="mimi-ojserkis" data-text="Mimi Ojserkis">Mimi Ojserkis</h1>
-      <ContactMe />
-    </div>
-  );
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1 id="mimi-ojserkis" data-text="Mimi Ojserkis">Mimi Ojserkis</h1>
+        <ContactMe />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ContactMe.js
+++ src/ContactMe.js
@@ -1,9 +1,56 @@
 import React from "react"
 
+// let width = window.innerWidth;
+// let height = window.innerHeight;
+
 class ContactMe extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            diffX: 0,
+            diffY: 0,
+            dragging: false,
+            styles: {}
+        }
+
+        this._dragStart = this._dragStart.bind(this);
+        this._dragging = this._dragging.bind(this);
+        this._dragEnd = this._dragEnd.bind(this);
+    }
+
+    _dragStart(e) {
+        this.setState({
+            diffX: e.screenX - e.currentTarget.getBoundingClientRect().left,
+            diffY: e.screenY - e.currentTarget.getBoundingClientRect().top,
+            dragging: true
+        });
+    }
+
+    _dragging(e) {
+
+        if(this.state.dragging) {
+            var left = e.screenX - this.state.diffX;
+            var top = e.screenY - this.state.diffY;
+    
+            this.setState({
+                styles: {
+                    left: left,
+                    top: top
+                }
+            });
+        }
+    }    
+
+    _dragEnd() {
+        this.setState({
+            dragging: false
+        });
+    }
+    
     render() {
         return(
-            <div className="window" id="contact-me-window">
+            <div className="window" id="contact-me-window" style={this.state.styles} onMouseDown={this._dragStart} onMouseMove={this._dragging} onMouseUp={this._dragEnd} >
                 <div className="title-bar">
                     <div className="title-bar-text">Contact Mimi</div>
                     <div className="title-bar-controls">
