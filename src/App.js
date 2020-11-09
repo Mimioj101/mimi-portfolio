ad2902440d9d3e64a2b30cdeaecd5f41596a4a22
Mimioj101 <mimioj@gmail.com>
2020-11-08 20:16:20 -0500

    positioned windows

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import ContactMe from './ContactMe.js'
 import Resume from './Resume.js'
 import NavBar from './Navbar.js'
 
-
-
-
 class App extends React.Component {
 
   render() {
     return (
       <div className="container">
         {/* <h1 id="mimi-ojserkis" data-text="Mimi Ojserkis">Mimi Ojserkis</h1> */}
-        <NavBar />
+        {/* <NavBar /> */}
         <ContactMe />
         <Resume />
       </div>
